30d086c75ee1671b26b8294614cbe557d5f9a1a5
Jamie <jamie.sam2006@gmail.com>
2017-05-02 00:21:40 -0700

    updating an changes

--- yumShare2/recipe/urls.py
+++ yumShare2/recipe/urls.py
@@ -7,14 +7,14 @@ urlpatterns = [
     # /recipe/
     url(r'^$', v.home, name='home'),
     # /recipe/detail/
-    url(r'^(?P<title>[\w.@+-]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'recipe/^(?P<title>[\w.@+-]+)/$', views.DetailView.as_view(), name='detail'),
     # /recipe/recipes/list/
-    url(r'^recipes/list/$', views.ListView.as_view(), name='list-recipe'),
+    url(r'^recipe/list/$', views.ListView.as_view(), name='list-recipe'),
     # /recipe/recipes/create/
     # url(r'^recipes/create/$', v.create_recipe, name='create-recipe'),
-    url(r'^recipes/create/$', views.RecipeCreateView.as_view(), name='create-recipe'),
+    url(r'^recipe/create/$', views.RecipeCreateView.as_view(), name='create-recipe'),
     # /recipe/recipes/title/
-    url(r'^recipes/(?P<title>[\w.@+-]+)/$', views.RecipeUpdate.as_view(), name='update-recipe'),
+    url(r'^recipe/(?P<title>[\w.@+-]+)/$', views.RecipeUpdateView.as_view(), name='update-recipe'),
     # /recipe/recipes/title/delete/
-    url(r'^recipes/(?P<title>[\w.@+-]+)/delete/$', views.RecipeDelete.as_view(), name='delete-recipe'),
+    url(r'^recipe/(?P<title>[\w.@+-]+)/delete/$', views.RecipeDelete.as_view(), name='delete-recipe'),
 ]
--- yumShare2/recipe/views.py
+++ yumShare2/recipe/views.py
@@ -1,9 +1,9 @@
 from django.db import transaction
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
-from django.contrib.auth import authenticate, login
 from django.views.generic import View
 from .models import RecipeInfo, Ingredient, Direction, Comment
 from .forms import IngredientFormSet, DirectionFormSet
@@ -32,11 +32,11 @@ class RecipeCreateView(CreateView):
     def get_context_data(self, **kwargs):
         data = super(RecipeCreateView, self).get_context_data(**kwargs)
         if self.request.POST:
-            data['ingredients'] = IngredientFormSet(self.request.POST)
-            data['directions'] = DirectionFormSet(self.request.POST)
+            data['ingredients'] = IngredientFormSet(self.request.POST, prefix='ingredient')
+            data['directions'] = DirectionFormSet(self.request.POST, prefix='direction')
         else:
-            data['ingredients'] = IngredientFormSet()
-            data['directions'] = DirectionFormSet()
+            data['ingredients'] = IngredientFormSet(prefix='ingredient')
+            data['directions'] = DirectionFormSet(prefix='direction')
         return data
 
     def form_valid(self, form):
@@ -49,6 +49,7 @@ class RecipeCreateView(CreateView):
             if ingredients.is_valid():
                 ingredients.instance = self.object
                 ingredients.save()
+            if directions.is_valid():
                 directions.instance = self.object
                 directions.save()
         return super(RecipeCreateView, self).form_valid(form)
@@ -62,9 +63,37 @@ class RecipeCreateView(CreateView):
     #context = {'form': form}
     #return render(request, 'recipe/create.html', context)
 
-class RecipeUpdate(UpdateView):
+class RecipeUpdateView(UpdateView):
     model = RecipeInfo
-    fields = ['title', 'category']
+    form_class = IngredientFormSet, DirectionFormSet
+    success_url = reverse_lazy('recipe:update-recipe')
+
+    def get_object(self):
+        pass
+
+
+    def get_context_data(self, **kwargs):
+        context = super(RecipeUpdateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['ingredients'] = IngredientFormSet(self.request.POST, instance=self.object)
+            context['directions'] = DirectionFormSet(self.request.POST, instance=self.object)
+        else:
+            context['ingredients'] = IngredientFormSet(instance=self.object)
+            context['directions'] = DirectionFormSet(instance=self.object)
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        ingredients = context['ingredients']
+        directions = context['directions']
+        if ingredients.is_valid() and directions.is_valid():
+            self.object = form.save()
+            ingredients.instance = self.object
+            ingredients.save()
+            directions.instance = self.object
+            directions.save()
+        return render(self.get_context_data(form=form))
+
 
 class RecipeDelete(DeleteView):
     model = RecipeInfo
