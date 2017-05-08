5de8a5ce7ad4a6997f262ce74ee25fb04cc9e361
Jamie <jamie.sam2006@gmail.com>
2017-05-07 20:20:17 -0700

    updating any changes made

---
+++ yumshare2/recipe/static/recipe/js/ajaxSearchBar.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Jamie on 5/4/2017.
+ */
+
+
+$(document).ready(function(){
+    function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
+
+
+    function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+    $('#searchBarMain').keyup(function(){
+    var word = $(this).val();
+        if(word.length > 0){
+            $.ajax({
+                url: "/yumshare/recipe/search/"+ word + '/',
+                type: "POST",
+                success: function(response){
+                    console.log(response)
+                }
+            });
+        }
+    });
+});
